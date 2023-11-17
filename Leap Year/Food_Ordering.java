93830e565c93096bd87fef63b27760ebc151a72c
drkcutie <derrick.binangbang@cit.edu>
2023-11-17 16:24:09 +0800

    THIS IS MY QUIZ SEURRR!

---
+++ Leap Year/Food_Ordering.java
@@ -0,0 +1,69 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Food_Ordering extends JFrame {
+    private JLabel lbFoods;
+    private JLabel lbDiscount;
+    private JCheckBox cPizza;
+    private JCheckBox cBurger;
+    private JCheckBox cFries;
+    private JCheckBox cSoftDrinks;
+    private JCheckBox cTea;
+    private JCheckBox cSundae;
+    private JRadioButton rbNone;
+    private JButton btnOrder;
+    private javax.swing.JPanel JPanel;
+    private JRadioButton rb5;
+    private JRadioButton rb10;
+    private JRadioButton rb15;
+    public Food_Ordering()
+    {
+
+        btnOrder.addActionListener(new ActionListener() {
+
+
+            public void actionPerformed(ActionEvent e) {
+                double sum = 0;
+                if(cPizza.isSelected())
+                {
+                    sum += 100;
+                }
+                if(cBurger.isSelected())
+                    sum+= 80;
+                if(cFries.isSelected())
+                    sum+= 65;
+                if(cSoftDrinks.isSelected())
+                    sum+=55;
+                if(cTea.isSelected())
+                    sum+= 50;
+                if(cSundae.isSelected())
+                    sum+= 40;
+
+                if(rb5.isSelected())
+                    sum = sum * .95;
+                else if(rb10.isSelected())
+                    sum = sum * .90;
+                else if(rb15.isSelected())
+                    sum = sum * .85;
+                if(sum == 0){
+                    JOptionPane.showMessageDialog(null, "Select something.");
+                    return;
+                }
+
+                JOptionPane.showMessageDialog(null,"The total price is PHP "+sum);
+            }
+        });
+    }
+    public static void main(String[] args) {
+
+        Food_Ordering app = new Food_Ordering();
+
+        app.setContentPane(app.JPanel);
+        app.setSize(600, 400);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Simple Calculator");
+        app.setVisible(true);
+    }
+}
+
---
+++ Leap Year/Simple_Calculator.java
@@ -0,0 +1,79 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Simple_Calculator extends JFrame {
+    private javax.swing.JPanel JPanel;
+    private JTextField tfNumber1;
+    private JTextField tfNumber2;
+    private JTextField tfResult;
+    private JComboBox cbOperations;
+    private JLabel lbNumber1;
+    private JLabel lbNumber2;
+    private JLabel lbResult;
+    private JButton btnCompute;
+
+    public Simple_Calculator()
+    {
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(tfNumber1.getText().equals("")||tfNumber2.getText().equals(""))
+                {
+                    JOptionPane.showMessageDialog(null,"Input 2 numbers.");
+                    return;
+                }
+                float first;
+                float second;
+                try {
+                    first = Float.parseFloat(tfNumber1.getText());
+                    second = Float.parseFloat(tfNumber2.getText());
+                }catch (Exception a)
+                {
+                    JOptionPane.showMessageDialog(null,"Not a valid input.");
+                    return;
+                }
+                float res;
+                if(cbOperations.getSelectedIndex() == 0)
+                {
+                    res = first + second;
+                    tfResult.setText(Float.toString(res));
+                }
+                else if (cbOperations.getSelectedIndex() == 1)
+                {
+                    res = first - second;
+                    tfResult.setText(String.valueOf(res));
+                }
+                else if (cbOperations.getSelectedIndex() == 2)
+                {
+                     res = first * second;
+                    tfResult.setText(String.valueOf(res));
+                }
+                else
+                {
+                    if(second == 0){
+                        JOptionPane.showMessageDialog(null,"Divisor cannot be 0");
+                        return;
+                    }
+
+                    res = first / second;
+
+                    tfResult.setText(String.valueOf(res));
+                }
+            }
+        });
+
+
+
+
+    }
+    public static void main(String[] args) {
+        Simple_Calculator app = new Simple_Calculator();
+
+        app.setContentPane(app.JPanel);
+        app.setSize(600, 400);
+        app.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        app.setTitle("Simple Calculator");
+        app.setVisible(true);
+    }
+}
